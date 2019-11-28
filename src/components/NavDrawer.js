8f4d3201d64bbfaba28db7b2f211d0aced9a710c
Reece Dunham <me@rdil.rocks>
2019-11-28 16:54:27 +0000

    support subpages

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -18,11 +18,12 @@
 
 import React from "react"
 import Drawer from "@material-ui/core/Drawer"
-import { makeStyles } from "@material-ui/core"
+import { makeStyles, Typography } from "@material-ui/core"
 import ListItem from "@material-ui/core/ListItem"
 import ListItemText from "@material-ui/core/ListItemText"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import Description from "@material-ui/icons/Description"
+import Divider from "@material-ui/core/Divider"
 import { Link, withRouter } from "react-router-dom"
 
 export default withRouter(props => {
@@ -33,30 +34,54 @@ export default withRouter(props => {
         link: {
             color: 'inherit',
             textDecoration: 'none'
+        },
+        title: {
+            textAlign: 'center',
+            margin: theme.spacing(1)
         }
     }))()
 
     const inputs = {
-        "/": "Home"
+        "/": {
+            "display": "Home"
+        }
     }
 
     let items = []
 
     for (const item in inputs) {
         items.push(
-            <ListItem button key={inputs[item]}>
+            <ListItem button key={inputs[item].display}>
                 <ListItemIcon>
                     <Description />
                 </ListItemIcon>
                 <Link to={item} className={styles.link}>
-                    <ListItemText primary={inputs[item]} />
+                    <ListItemText primary={`    ${inputs[item].display}`} />
                 </Link>
             </ListItem>
         )
+        if (item.hasOwnProperty("subpages")) {
+            for (const subpage in item.subpages) {
+                items.push(
+                    <ListItem button key={inputs[item].subpages[subpage].display}>
+                        <ListItemIcon>
+                            <Description />
+                        </ListItemIcon>
+                        <Link to={subpage} className={styles.link}>
+                            <ListItemText primary={inputs[item].subpages[subpage].display} />
+                        </Link>
+                    </ListItem>
+                )
+            }
+        }
     }
 
     return (
         <Drawer open={props.open} onClose={props.notifyParentOfClose}>
+            <Typography variant="subtitle2" className={styles.title}>
+                Pages
+            </Typography>
+            <Divider />
             <div className={styles.list} role="presentation">
                 {items}
             </div>
